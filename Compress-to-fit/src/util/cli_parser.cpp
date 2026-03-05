a053627c72c0f105aab6c3ab1d68aa2ba507bcd1
Troan34 <roanbukaci5@gmail.com>
2026-03-05 22:31:28 +0100

    Feat: Implementing tokenize_option

--- Compress-to-fit/src/util/cli_parser.cpp
+++ Compress-to-fit/src/util/cli_parser.cpp
@@ -1,23 +1,81 @@
 #include "cli_parser.hpp"
+#include <charconv>
+#include <fstream>
 
 namespace parser
 {
-	std::expected<Token, ErrorType> tokenize_option(const std::string& option)
+
+std::expected<Token, ErrorType> tokenize_option(const std::string& option)
+{
+	auto token_string = option.substr(0, option.find(' ') + 1);
+	auto token_value = option.substr(option.find(' ') + 1, std::string::npos);
+
+	if (token_string.size() == option.size())//we have not found a ' ' character <-> find() returned npos
+		return std::unexpected(ErrorType::SYNTAX_ERROR);
+
+	TokenType token_type;
+
+	//Find TokenType
+	if (token_string == token_strings[static_cast<std::size_t>(TokenType::COMPRESSION_PRESET)])
 	{
-		auto token_string = option.substr(0, option.find(' ') + 1);
-		auto token_value = option.substr(option.find(' ') + 1, std::string::npos);
+		token_type = TokenType::COMPRESSION_PRESET;
+	}
+	else if (token_string == token_strings[static_cast<std::size_t>(TokenType::FILENAME_IN)])
+	{
+		token_type = TokenType::FILENAME_IN;
+	}
+	else if (token_string == token_strings[static_cast<std::size_t>(TokenType::FILENAME_OUT)])
+	{
+		token_type = TokenType::FILENAME_OUT;
+	}
+	else
+		return std::unexpected(ErrorType::SYNTAX_ERROR);
 
-		if (token_string.size() == option.size())//we have not found a ' ' character, find() returned npos
-			return std::unexpected(ErrorType::SYNTAX_ERROR);
+	ValueType value;
+	//Find token_value and do error checking
+	switch (token_type)
+	{
+	case TokenType::FILENAME_IN:
+	{
+		//Test if we can find and access the input file
+		auto& value_ref = std::get<fs::path>(value);
 
-		//Find TokenType
-		if(token_string)
+		if (!fs::exists(token_value))
+			return std::unexpected(ErrorType::PATH_NOT_FOUND);
 
+		if (std::ifstream file(token_value); !file.is_open())
+			return std::unexpected(ErrorType::PATH_NOT_ACCESSIBLE);
 
+		value_ref = token_value;
+	}
+	break;
+	case TokenType::FILENAME_OUT:
+	{
+		//Test if we can write a file to the given dir
+		auto value_ref = std::get<fs::path>(value);
+
+		auto test_path = value_ref.parent_path() / ".test_writability";
+		std::ofstream test_file{test_path};
+		if (!test_file.is_open())
+			return std::unexpected(ErrorType::PATH_NOT_ACCESSIBLE);
+	}
+	break;
+	case TokenType::COMPRESSION_PRESET:
+	{
+		//Convert and check the compression preset
+		auto& value_ref = std::get<size_t>(value);
+		auto res = std::from_chars(token_value.data(), token_value.data() + token_value.size(), value_ref);
+
+		if (res.ec != std::errc() or (value_ref > CompPreset::COMP_MAX or value_ref < CompPreset::NO_COMP))//if not a number or outside of our range
+			return std::unexpected(ErrorType::VALUE_ERROR);
+	}
+	break;
 	}
 
-	Token::Token(TokenType type_, ValueType value_)
-	{
-	}
+}
+
+Token::Token(TokenType type_, ValueType value_)
+{
+}
 
 }//namespace parser
--- Compress-to-fit/src/util/cli_parser.hpp
+++ Compress-to-fit/src/util/cli_parser.hpp
@@ -13,6 +13,8 @@ cli_parser.hpp - parse and categorize the cli input
 namespace parser
 {
 	namespace fs = std::filesystem;
+	using ValueType = std::variant<std::size_t, fs::path, bool>;
+	
 
 	enum class TokenType
 	{
@@ -20,7 +22,6 @@ namespace parser
 		FILENAME_OUT,
 		COMPRESSION_PRESET,
 	};
-
 	//HAS to follow the same order of the TokenType enums
 	static constexpr std::array<std::string_view, 3> token_strings =
 	{
@@ -34,19 +35,34 @@ namespace parser
 	/// </summary>
 	enum class ErrorType
 	{
-		VALUE_MISSING,
-		VALUE_INCORRECT,
+		VALUE_ERROR,
 		SYNTAX_ERROR,
 		OPTION_UNAVAILABLE,
+		PATH_NOT_FOUND,
+		PATH_NOT_ACCESSIBLE,
 	};
 
-
+	//unscoped because having to add static_cast becomes annoying
+	enum CompPreset
+	{
+		NO_COMP,
+		COMP_1,
+		COMP_2,
+		COMP_3,
+		COMP_4,
+		COMP_5,
+		COMP_6,
+		COMP_7,
+		COMP_8,
+		COMP_MAX,
+	};
 	
-
-
+	/// <summary>
+	/// Represents a cli option
+	/// </summary>
 	class Token
 	{
-	using ValueType = std::variant<std::size_t, fs::path, bool>;
+	
 
 	public:
 		TokenType type;
@@ -57,10 +73,11 @@ namespace parser
 		 ValueType value;
 	};
 
-
+	/// <summary>
+	/// Constructs a Token given a string that looks like this "-<option> <value>"
+	/// </summary>
+	/// <returns>an std::expected with E as parser::ErrorType</returns>
 	std::expected<Token, ErrorType> tokenize_option(const std::string& option);
-
-
 
 
 }//namespace parser
