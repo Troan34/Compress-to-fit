bee6aeba83f159a2744963c54b01f442fd4acd1a
Troan34 <roanbukaci5@gmail.com>
2026-03-06 22:06:41 +0100

    WIP: finishing up the parser

--- Compress-to-fit/src/util/cli_parser.cpp
+++ Compress-to-fit/src/util/cli_parser.cpp
@@ -1,11 +1,27 @@
 #include "cli_parser.hpp"
 #include <charconv>
 #include <fstream>
+#include <cassert>
 
 namespace parser
 {
 
-std::expected<Token, ErrorType> tokenize_option(const std::string& option)
+Token::Token(TokenType type_, ValueType value_)
+	:type(type_), value(value_)
+{
+}
+
+Token::Token(const std::string& option)
+{
+	auto res = lex(option);
+	if (res)
+	{
+		type = res.value().type;
+		value = res.value().value;
+	}
+}
+
+std::expected<Token, ErrorType> lex(const std::string& option)
 {
 	auto token_string = option.substr(0, option.find(' ') + 1);
 	auto token_value = option.substr(option.find(' ') + 1, std::string::npos);
@@ -27,6 +43,14 @@ std::expected<Token, ErrorType> tokenize_option(const std::string& option)
 	else if (token_string == token_strings[static_cast<std::size_t>(TokenType::FILENAME_OUT)])
 	{
 		token_type = TokenType::FILENAME_OUT;
+	}
+	else if (token_string == token_strings[static_cast<std::size_t>(TokenType::N_FILES)])
+	{
+		token_type = TokenType::N_FILES;
+	}
+	else if (token_string == token_strings[static_cast<std::size_t>(TokenType::SIZE_FILES)])
+	{
+		token_type = TokenType::SIZE_FILES;
 	}
 	else
 		return std::unexpected(ErrorType::SYNTAX_ERROR);
@@ -70,12 +94,94 @@ std::expected<Token, ErrorType> tokenize_option(const std::string& option)
 			return std::unexpected(ErrorType::VALUE_ERROR);
 	}
 	break;
+	case TokenType::N_FILES:
+	{
+		int value_temp;
+		auto res = std::from_chars(token_value.data(), token_value.data() + token_value.size(), value_temp);
+
+		if (res.ec != std::errc() or (value_temp > N_FILES_LIMIT or value_temp < 0))//if negative or over N_FILES_LIMIT
+			return std::unexpected(ErrorType::VALUE_ERROR);
+		
+		std::get<size_t>(value) = value_temp;
+	}
+	break;
+	case TokenType::SIZE_FILES:
+	{
+		int value_temp;
+		auto res = std::from_chars(token_value.data(), token_value.data() + token_value.size(), value_temp);
+
+		if (res.ec != std::errc() or (value_temp < SIZE_FILES_MIN))//if negative or under SIZE_FILES_MIN
+			return std::unexpected(ErrorType::VALUE_ERROR);
+
+		std::get<size_t>(value) = value_temp;
+	}
 	}
 
+	return Token{ token_type, token_value };
 }
 
-Token::Token(TokenType type_, ValueType value_)
+void throw_error(ErrorType error, const std::string& error_option)
 {
+	switch (error)
+	{
+	case parser::ErrorType::NO_ERROR:
+		break;
+	case parser::ErrorType::VALUE_ERROR:
+		throw std::runtime_error(error_option + "< " + ERR_STRING::VALUE);
+		break;
+	case parser::ErrorType::SYNTAX_ERROR:
+		throw std::runtime_error(error_option + "< " + ERR_STRING::SYNTAX);
+		break;
+	case parser::ErrorType::OPTION_UNAVAILABLE:
+		throw std::runtime_error(error_option + "< " + ERR_STRING::OPTION_UNAVAILABLE);
+		break;
+	case parser::ErrorType::PATH_NOT_FOUND:
+		throw std::runtime_error(error_option + "< " + ERR_STRING::PATH_NOT_FOUND);
+		break;
+	case parser::ErrorType::PATH_NOT_ACCESSIBLE:
+		throw std::runtime_error(error_option + "< " + ERR_STRING::PATH_NOT_ACCESSIBLE);
+		break;
+	default:
+		break;
+	}
+}
+
+Options parse(int argc, char* argv[])
+{
+	Options options;
+	//starting at 1 means we ignore the first (name of program)
+	for (int index = 1; index < argc; index += 2)
+	{
+		std::string option{ argv[index] };
+		option += argv[index + 1];
+		
+		auto token = lex(option);
+
+		if (token)
+		{
+			switch (token.value().get_type())
+			{
+			case TokenType::FILENAME_IN:
+				options.filename_in = std::get<fs::path>(token.value().get_value());
+				break;
+			case TokenType::FILENAME_OUT:
+				options.filename_out = std::get<fs::path>(token.value().get_value());
+				break;
+			case TokenType::COMPRESSION_PRESET:
+				options.preset = std::get<size_t>(token.value().get_value());
+				break;
+			case TokenType::N_FILES:
+				options.n_files = std::get<size_t>(token.value().get_value());
+				break;
+			case TokenType::SIZE_FILES:
+				options.size_files = std::get<size_t>(token.value().get_value());
+				break;
+			default:
+				assert(false == true);
+				break;
+			}
+		}
+	}
 }
 
 }//namespace parser
--- Compress-to-fit/src/util/cli_parser.hpp
+++ Compress-to-fit/src/util/cli_parser.hpp
@@ -9,25 +9,33 @@ cli_parser.hpp - parse and categorize the cli input
 #include <expected>
 #include <array>
 
+static constexpr size_t N_FILES_LIMIT = 1'000;
+static constexpr size_t SIZE_FILES_MIN = 512;
 
 namespace parser
 {
 	namespace fs = std::filesystem;
 	using ValueType = std::variant<std::size_t, fs::path, bool>;
-	
+
 
 	enum class TokenType
 	{
 		FILENAME_IN,
 		FILENAME_OUT,
 		COMPRESSION_PRESET,
+		N_FILES,
+		SIZE_FILES,
+		NO_TYPE = 696969,//we don't want this to be an index
 	};
 	//HAS to follow the same order of the TokenType enums
-	static constexpr std::array<std::string_view, 3> token_strings =
+	static constexpr std::array<std::string_view, 5> token_strings =
 	{
 		"-i",
 		"-o",
-		"-preset"
+		"-preset",
+		"-n_files",
+		"-size_files",
+
 	};
 
 	/// <summary>
@@ -35,12 +43,22 @@ namespace parser
 	/// </summary>
 	enum class ErrorType
 	{
+		NO_ERROR,
 		VALUE_ERROR,
 		SYNTAX_ERROR,
 		OPTION_UNAVAILABLE,
 		PATH_NOT_FOUND,
 		PATH_NOT_ACCESSIBLE,
 	};
+	
+	namespace ERR_STRING
+	{
+		static constexpr const char* VALUE = "\033[31merr\033[0m: this is an incorrect value for this option.";
+		static constexpr const char* SYNTAX = "\033[31merr\033[0m: the syntax for this option is incorrect.";
+		static constexpr const char* OPTION_UNAVAILABLE = "\033[31merr\033[0m: this option is unavailable in this context.";
+		static constexpr const char* PATH_NOT_FOUND = "\033[31merr\033[0m: this path could not be found, make sure to double quote (\") around your path if there are spaces";
+		static constexpr const char* PATH_NOT_ACCESSIBLE = "\033[31merr\033[0m: this path could not be accessed, the program may not have some required privileges";
+	}
 
 	//unscoped because having to add static_cast becomes annoying
 	enum CompPreset
@@ -62,22 +80,47 @@ namespace parser
 	/// </summary>
 	class Token
 	{
-	
-
 	public:
-		TokenType type;
-		
+	
 		Token(TokenType type_, ValueType value_);
+		Token(const std::string& option);
+
+		inline ValueType get_value() const noexcept
+		{
+			return value;
+		}
+
+		inline TokenType get_type() const noexcept
+		{
+			return type;
+		}
 
 	private:
-		 ValueType value;
+		ValueType value;
+		TokenType type = TokenType::NO_TYPE;
 	};
 
-	/// <summary>
-	/// Constructs a Token given a string that looks like this "-<option> <value>"
-	/// </summary>
-	/// <returns>an std::expected with E as parser::ErrorType</returns>
-	std::expected<Token, ErrorType> tokenize_option(const std::string& option);
+	//Constructs a Token given a string that looks like this "-<option> <value>"
+	std::expected<Token, ErrorType> lex(const std::string& option);
 
+	void throw_error(ErrorType error, const std::string& error_option);
+
+	/// <summary>
+	/// All the options available
+	/// </summary>
+	struct Options
+	{
+		fs::path filename_in;
+		fs::path filename_out;
+		size_t preset = COMP_5;
+		size_t n_files = 1;
+		size_t size_files;
+	};
+	
+	/// <summary>
+	/// Parse the cli, fully manages error checking
+	/// </summary>
+	/// <returns>An Option type</returns>
+	Options parse(int argc, char* argv[]);
 
 }//namespace parser
