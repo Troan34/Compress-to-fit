caaf3130980aa44b87ff893d87236609959596f0
Troan34 <roanbukaci5@gmail.com>
2026-07-11 23:41:07 +0200

    Hello, Qt!

--- Compress-to-fit/Compress-to-fit.cpp
+++ Compress-to-fit/Compress-to-fit.cpp
@@ -1,6 +1,8 @@
 ﻿
 #include <mio/mmap.hpp>
 #include "src/util/macros.hpp"
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
 import util;
 
@@ -14,6 +16,19 @@ namespace fs = std::filesystem;
 
 int main(int argc, char* argv[])
 {
+
+	QGuiApplication app(argc, argv);
+
+	QQmlApplicationEngine engine;
+	QObject::connect(
+		&engine,
+		&QQmlApplicationEngine::objectCreationFailed,
+		&app,
+		[]() { QCoreApplication::exit(-1); },
+		Qt::QueuedConnection);
+	engine.load(QUrl(u"qrc:/Main.qml"_qs));
+	return QGuiApplication::exec();
+
 	WIN_CALL(SetConsoleOutputCP(CP_UTF8));
 
 	auto const options = parser::parse(argc, argv);
---
+++ Design/Main.qml
@@ -0,0 +1,10 @@
+import QtQuick
+import QtQuick.Window
+
+Window {
+    height: 500
+    width: 500
+    visible: true
+}
+
+
